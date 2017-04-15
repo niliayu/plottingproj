45065ab0535de3bae1f7a620320fb380d271696d
Ailin Yu <axy98@case.edu>
2017-04-15 15:30:23 -0400

    Initiali commit

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿// Create instance of base window
+
+namespace ConsoleApplication
+{
+    public static class GlobalVars
+    {
+        public static string[] Buildings = {"olin", "white"};
+    }
+
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Windows.Forms.Application.Run(new Window());
+        }
+    }
+
+}
---
+++ Window.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+using System.Data;
+
+// Creates base window for application, calls and refreshes graphs
+
+namespace ConsoleApplication
+{
+    public class Window : Form
+    {
+        private System.ComponentModel.Container components = null;
+        private Label label1;
+        private ComboBox buildingSelect;
+
+        public Window()
+        {
+            this.Size = new System.Drawing.Size(600,700);
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            components = new System.ComponentModel.Container();
+
+            //Add labels
+            label1 = new Label();
+            label1.Text = "Eventually this might be great";
+            Controls.Add(label1);
+
+            //Add dropdown menus
+            buildingSelect = new ComboBox();
+            string[] buildings = GlobalVars.Buildings;
+            buildingSelect.Items.AddRange(buildings);
+            Controls.Add(buildingSelect);
+        }
+    }
+}
