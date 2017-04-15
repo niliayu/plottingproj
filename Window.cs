e38132be5fefd970e3c03e2578d6b9ad4bda31f7
Ailin Yu <axy98@case.edu>
2017-04-15 16:21:32 -0400

    Added plot

--- Window.cs
+++ Window.cs
@@ -1,36 +1,75 @@
 ﻿using System.Windows.Forms;
-using System.Data;
-
+using System.Drawing;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
 // Creates base window for application, calls and refreshes graphs
 
 namespace ConsoleApplication
 {
     public class Window : Form
     {
-        private System.ComponentModel.Container components = null;
+        // Containers and grids
+ //       private System.ComponentModel.Container components = null;
+
+        // Components
         private Label label1;
         private ComboBox buildingSelect;
+        private PlotView plot;
 
         public Window()
         {
-            this.Size = new System.Drawing.Size(600,700);
+            InitializeForm();
             InitializeComponents();
+        }
+
+        private void InitializeForm()
+        {
+            Size = new System.Drawing.Size(600,700);
+            Name = "Plotter";
+
         }
 
         private void InitializeComponents()
         {
-            components = new System.ComponentModel.Container();
-
-            //Add labels
-            label1 = new Label();
-            label1.Text = "Eventually this might be great";
-            Controls.Add(label1);
 
             //Add dropdown menus
             buildingSelect = new ComboBox();
             string[] buildings = GlobalVars.Buildings;
             buildingSelect.Items.AddRange(buildings);
+            buildingSelect.Location = new Point(300,500);
             Controls.Add(buildingSelect);
+
+            //Add plots
+            InitializePlot();
+            var mainPlot = new PlotModel {Title = "Main"};
+            ScatterSeries temp_ = new ScatterSeries {MarkerType = MarkerType.Circle};
+
+            for (int i = 0; i < 100; i++)
+            {
+                temp_.Points.Add(new ScatterPoint(i, i+1));
+            }
+
+            mainPlot.Series.Add(temp_);
+            plot.Model = mainPlot;
+            Controls.Add(plot);
         }
+
+        // for now initializes one main plot; eventually have option for many
+        private void InitializePlot()
+        {
+            plot = new PlotView();
+            SuspendLayout();
+
+            plot.Dock = DockStyle.Fill;
+            plot.Location = new Point(100, 100);
+            plot.Name = "MainPlot";
+            plot.Size = new Size(400, 400);
+            plot.TabIndex = 0;
+            plot.Text = "MainPlot";
+
+        }
+
+
     }
 }
